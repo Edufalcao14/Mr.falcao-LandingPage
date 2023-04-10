0fdf0978d300b79db108d6f0446c908200b857dc
edufalcao14 <eduardo.sampaio@student.vinci.be>
2023-04-10 04:25:56 +0200

    fix error

--- components/FailModal/index.jsx
+++ components/FailModal/index.jsx
@@ -1,5 +1,5 @@
 import { FiXSquare } from "react-icons/fi";
-import Button from "../button";
+import Button from "../Button";
 import styles from "./fail.module.scss";
 
 export const FailModal = ({ closeModal }) => {
@@ -7,9 +7,9 @@ export const FailModal = ({ closeModal }) => {
     <div className={styles.overlay} onClick={closeModal}>
       <div className={styles.modalContainer}>
         <FiXSquare />
-        <p>Votre demande de prise de rendez-vous n'a pas pu être envoyée, veuillez réessayer.</p>
+        <p>Votre demande de prise de rendez-vous n`&apos`a pas pu être envoyée, veuillez réessayer.</p>
         <div className={styles.okButtonContainer} onClick={closeModal}>
-          <Button title="fechar" />
+          <Button title="fechar" kind="1" />
         </div>
       </div>
     </div>
